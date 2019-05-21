46dd395e8090c57ae4394f188214b15dd42a538b
Lisa Miller <lisa.miller@hs-weingarten.de>
2019-05-21 16:32:05 +0200

    build test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,130 @@
 <template>
+  <div id="background">
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> Welcome </h1> 
+    <h3> to NY Times most viewed articles </h3> 
+
+    <div v-if="articles!=[]"> 
+      <Article v-bind:articles="articles" v-bind:articleIndex="index"/>
+    </div>
+    <div v-else>
+      <h6> Sorry, no articles fetched. </h6>
+    </div>
+    
+    <button id="prev" @click="previous()"> prev </button>
+    <button id="next" @click="next()"> next </button>
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Article from './components/Article.vue';
+import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Article
+  },
+  data() {
+    return {
+      index: 0,
+      articles: [],
+      errors: []
+    }
+  },
+  mounted() {
+    console.log("mounted"),
+    axios.get("https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=pPAgGA1VOUKtKDeBbG7LYE7Yro5xKSJU")
+      .then(response => {
+        this.articles = response.data.results;
+        console.log(response.data.results);
+      });
+  },
+  methods: {
+    next() {
+      this.index++;
+      console.log('cur index: ', this.index);
+    },
+    previous() {
+      this.index--;
+      console.log('cur index: ', this.index);
+    }
   }
 }
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Dawning+of+a+New+Day&display=swap");
+
+body {
+  background-image: url(./assets/IMG_0803-1.png);
+  background-size: 100vw 100vh;
+}
+
+h1 {
+  font-size: 4.6em;
+}
+
+h3 {
+  margin-top: -1em;
+  font-family: "Dawning of a New Day";
+  font-size: 2em;
+  font-weight: 100;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 100, 0.6);
+  width: 100%;
+  height: 100%;
+}
+
+button {
+  background-color: transparent;
+  color: rgba(255, 255, 255, 0.7);;
+  border-style: solid;
+  border-width: 1px;
+  border-color: rgba(255, 255, 255, 0.4);
+  margin: 10px;
+  width: 150px;
+  height: 150px;
+  border-radius: 100px;
+  transition: 2s ease-out;
+  top: 50vh;
+}
+
+#prev {
+  position: fixed;
+  text-align: right;
+  left: -100px;
+}
+
+#next {
+  position: fixed;
+  text-align: left;
+  right: -100px;
+}
+
+button:hover {
+  /*background: rgba(255, 255, 255, 0.3);
+  color: rgba(255, 255, 255, 1);*/
+  background: rgba(0, 0, 0, 0.3);
+  border-color: rgba(0, 0, 0, 0);
+  transition: 1s ease-in;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Article.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="article">
+    <p>{{ articles[articleIndex].title }}</p>
+    <p> {{ articles[articleIndex].abstract }} </p>
+    <p> {{ articles[articleIndex].section }} </p>
+    <a :href="articles[articleIndex]['url']" target="_blanc"> read more </a>
+    </br>
+    <img :src="articles[articleIndex].media[0]['media-metadata'][2]['url']" >
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Article',
+  props: {
+    articleIndex: Number,
+    articles: Array
+  },
+  data() {
+    return {
+      img: ""
+    }
+  },
+  methods: {
+    getImg(index) {
+      this.img = articles[index].media[0][2];
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.article {
+  position: relative;
+  background-color: rgba(2300, 255, 255, 0.8);
+  width: 50vw;
+  height: 50vh;
+  box-shadow: 0px 0px 100px -50px rgba(255,255,255,1);
+  color: rgb(10, 10, 10);
+}
+</style>
